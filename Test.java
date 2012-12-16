351515e96321a560bdb22ca9bc5210b5e030d9d4
ueponmasuta <ueponmasuta@gmail.com>
2012-12-16 12:32:24 +0900

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,31 @@
-public class Test{
+interface X{
+  void foo();
+}
+interface Y{
+  void bar();
+}
+interface Z extends X, Y{
+  void hoge{};
+}
+
+abstract class A implements Z{
+  public void foo(){
+    System.out.println("A");
+  }
+}
+class B extends A{
+  public void foo(){
+    System.out.println("B");
+  }
+  public void hoge(){
+    System.out.println("C");
+  }
+}
+
+
+class Test{
   public static void main(Sttring[] args){
-    System.out.pritnln("A");
+    X b = new B();
+    b.foo();
   }
 }
